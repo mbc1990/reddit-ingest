70e337b37db88042aba83d8c2fa08f4b0d2eda30
mbc <malcolmbalch@gmail.com>
2018-02-04 13:05:21 -0500

    Parse comment tree

--- reddit_ingester.go
+++ reddit_ingester.go
@@ -1,6 +1,8 @@
 package main
 
 import "log"
+
+// import "strconv"
 import "net/http"
 import "encoding/json"
 import "encoding/base64"
@@ -9,6 +11,7 @@ import "bytes"
 import "io/ioutil"
 import "fmt"
 
+// TODO: Replace this with ResponsePrimative
 type SubredditResponse struct {
 	Data struct {
 		Children []struct {
@@ -20,8 +23,17 @@ type SubredditResponse struct {
 	}
 }
 
-// TODO: This one is trickier because it needs to recurse over a comment tree
-type CommentsResponse struct {
+// The reddit api returns trees of these objects which are identified by their "kind" field
+type ResponsePrimative struct {
+	Kind string
+	Data struct {
+		Created_UTC int
+		Body        string
+		Title       string
+		Selftext    string
+		Children    *[]ResponsePrimative
+		Replies     *ResponsePrimative
+	}
 }
 
 type JobInfo struct {
@@ -37,10 +49,33 @@ type RedditIngester struct {
 	AccessToken string
 }
 
+// Parses a response tree for comments and writes them to postgres
+func (r *RedditIngester) ParseTreeForComments(tree *ResponsePrimative) {
+	// TODO: This method just prints the comments right now. Next step is writing them to postgres
+	switch tree.Kind {
+	case "t3":
+		fmt.Println(tree.Data.Title)
+		fmt.Println(tree.Data.Selftext)
+	case "t1":
+		fmt.Println(tree.Data.Body)
+		// Don't recurse if it's an empty struct (leaf node)
+		if *tree.Data.Replies != (ResponsePrimative{}) {
+			r.ParseTreeForComments(tree.Data.Replies)
+		}
+	case "Listing":
+		for _, child := range *tree.Data.Children {
+			r.ParseTreeForComments(&child)
+		}
+	default:
+		fmt.Println("Unexpected object type: " + tree.Kind)
+	}
+}
+
 func (r *RedditIngester) Worker() {
 	for info := range r.WorkQueue {
+		// TODO: Refactor request construction out of the if/then cases
 		if info.PageType == "subreddit" {
-			url := "https://oauth.reddit.com/r/cryptocurrencies"
+			url := "https://oauth.reddit.com/r/boston"
 			req, _ := http.NewRequest("GET", url, nil)
 			req.Header.Add("Authorization", "Bearer "+r.AccessToken)
 			req.Header.Add("User-Agent", r.Conf.ClientId+"by "+r.Conf.Username)
@@ -54,22 +89,39 @@ func (r *RedditIngester) Worker() {
 			subredditResponse := new(SubredditResponse)
 			json.Unmarshal(body, &subredditResponse)
 			for i, story := range subredditResponse.Data.Children {
-				fmt.Println(story)
 				url := story.Data.Permalink
 				ji := new(JobInfo)
 				ji.URL = url
 				ji.PageType = "comments"
 				r.WorkQueue <- *ji
-				if i == 1 {
+				// TODO: Testing, remove
+				if i == 0 {
 					break
 				}
 			}
 
 		} else if info.PageType == "comments" {
-			fmt.Println(info)
-			// TODO: Traverse comments trees
-			// TODO: Extract time, content, unique id
-			// TODO: Write to postgres if unique
+			url := "https://oauth.reddit.com" + info.URL
+			fmt.Println("Getting comments for " + url)
+			req, _ := http.NewRequest("GET", url, nil)
+			req.Header.Add("Authorization", "Bearer "+r.AccessToken)
+			req.Header.Add("User-Agent", r.Conf.ClientId+"by "+r.Conf.Username)
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				panic(err)
+			}
+			defer resp.Body.Close()
+			body, _ := ioutil.ReadAll(resp.Body)
+			commentResponse := make([]ResponsePrimative, 0)
+			json.Unmarshal(body, &commentResponse)
+
+			// A comment response is an array of trees, so send each off
+			// to the recursive tree parser
+			for _, topLevelNode := range commentResponse {
+				r.ParseTreeForComments(&topLevelNode)
+			}
+
 		} else {
 			log.Fatal("Unexpected job type")
 		}
@@ -96,13 +148,12 @@ type AuthResponse struct {
 	Error        int
 }
 
-// Goes through the reddit OAuth flow
+// Goes through the reddit Basic Authentication flow
 func (r *RedditIngester) Authenticate() {
 	url := "https://www.reddit.com/api/v1/access_token"
 	client := &http.Client{}
 	bodyToSend := bytes.NewBuffer([]byte("grant_type=client_credentials&\\device_id=1"))
 	req, _ := http.NewRequest("POST", url, bodyToSend)
-
 	toEncode := []byte(r.Conf.ClientId + ":" + r.Conf.Secret)
 	toSend := base64.StdEncoding.EncodeToString(toEncode)
 	req.Header.Add("Authorization", "Basic "+toSend)
@@ -113,7 +164,6 @@ func (r *RedditIngester) Authenticate() {
 	}
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
-
 	authResp := new(AuthResponse)
 	json.Unmarshal(body, &authResp)
 	r.AccessToken = authResp.Access_token
